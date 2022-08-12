6d5d9fcf1ed9f346d7744cbaf080ebe326e90534
Darya <dashulya18@mail.ru>
2022-08-12 18:33:05 +0300

    Решение задач по семинару 1

---
+++ Zad1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32 (Console.ReadLine ());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32 (Console.ReadLine ());
+
+if (firstNumber > secondNumber)
+{
+   Console.WriteLine("max = " + firstNumber);
+}
+else
+{
+   Console.WriteLine("max = " + secondNumber);
+}
---
+++ Zad3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " - четное");
+}    
+else
+{
+    Console.WriteLine("Число " + number + " не является четным");
+}
